3f0a1f834367f7ae359e14fac43cd68ee603195f
verengan122 <Crysis00!>
2022-05-24 20:19:00 +0300

    add credentials.properties

--- src/test/java/config/CredentialsConfig.java
+++ src/test/java/config/CredentialsConfig.java
@@ -3,7 +3,7 @@ package config;
 import org.aeonbits.owner.Config;
 
 @Config.Sources({
-        "classpath:credentials.properties"
+        "classpath:credentials-examples.properties"
 })
 public interface CredentialsConfig extends Config {
 
